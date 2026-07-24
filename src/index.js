90d57a1b2a1efc9aaca46dac1fca450ca6288ac8
Erin Wilson <ewilson001500@gmail.com>
2026-07-24 16:43:36 -0500

    Added more JS for the drop-down list

--- src/index.js
+++ src/index.js
@@ -2,31 +2,56 @@ function updateTime() {
   //Houston
   let houstonElement = document.querySelector("#houston");
 
-  let houstonDate = houstonElement.querySelector(".date");
-  let houstonCurrentDate = moment().format("MMMM Do, YYYY");
+  if (houstonElement) {
+    let houstonDate = houstonElement.querySelector(".date");
+    let houstonCurrentDate = moment().format("MMMM Do, YYYY");
 
-  let houstonTime = houstonElement.querySelector(".time");
-  let houstonCurrentTime = moment()
-    .tz("America/Chicago")
-    .format("h:mm:ss [<small>]A[</small>]");
+    let houstonTime = houstonElement.querySelector(".time");
+    let houstonCurrentTime = moment()
+      .tz("America/Chicago")
+      .format("h:mm:ss [<small>]A[</small>]");
 
-  houstonDate.innerHTML = houstonCurrentDate;
-  houstonTime.innerHTML = houstonCurrentTime;
+    houstonDate.innerHTML = houstonCurrentDate;
+    houstonTime.innerHTML = houstonCurrentTime;
+  }
 
   //Sydney
   let sydneyElement = document.querySelector("#sydney");
 
-  let sydneyDate = sydneyElement.querySelector(".date");
-  let sydneyCurrentDate = moment().format("MMMM Do, YYYY");
+  if (sydneyElement) {
+    let sydneyDate = sydneyElement.querySelector(".date");
+    let sydneyCurrentDate = moment().format("MMMM Do, YYYY");
 
-  let sydneyTime = sydneyElement.querySelector(".time");
-  let sydneyCurrentTime = moment()
-    .tz("Australia/Sydney")
+    let sydneyTime = sydneyElement.querySelector(".time");
+    let sydneyCurrentTime = moment()
+      .tz("Australia/Sydney")
+      .format("h:mm:ss [<small>]A[</small>]");
+
+    sydneyDate.innerHTML = sydneyCurrentDate;
+    sydneyTime.innerHTML = sydneyCurrentTime;
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityDate = moment().format("MMMM Do, YYYY");
+  let cityTime = moment()
+    .tz(cityTimeZone)
     .format("h:mm:ss [<small>]A[</small>]");
-
-  sydneyDate.innerHTML = sydneyCurrentDate;
-  sydneyTime.innerHTML = sydneyCurrentTime;
+  let cityElement = document.querySelector("#cities");
+  cityElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityDate}</div>
+          </div>
+          <div class="time">${cityTime}</div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let selectCityElement = document.querySelector("#city");
+selectCityElement.addEventListener("change", updateCity);
