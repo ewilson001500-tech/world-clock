fa908fc2b4fa3c3742a8f2d4552c40768b068e91
Erin Wilson <ewilson001500@gmail.com>
2026-07-24 20:16:14 -0500

    Added two new cities and a link to the homepage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 function updateTime() {
   //Houston
+  let currentDate = moment().format("MMMM Do, YYYY");
+  let timeFormat = "h:mm:ss [<small>]A[</small>]";
   let houstonElement = document.querySelector("#houston");
 
   if (houstonElement) {
     let houstonDate = houstonElement.querySelector(".date");
-    let houstonCurrentDate = moment().format("MMMM Do, YYYY");
-
     let houstonTime = houstonElement.querySelector(".time");
-    let houstonCurrentTime = moment()
-      .tz("America/Chicago")
-      .format("h:mm:ss [<small>]A[</small>]");
+    let houstonCurrentTime = moment().tz("America/Chicago").format(timeFormat);
 
-    houstonDate.innerHTML = houstonCurrentDate;
+    houstonDate.innerHTML = currentDate;
     houstonTime.innerHTML = houstonCurrentTime;
   }
 
@@ -20,15 +18,35 @@ function updateTime() {
 
   if (sydneyElement) {
     let sydneyDate = sydneyElement.querySelector(".date");
-    let sydneyCurrentDate = moment().format("MMMM Do, YYYY");
-
     let sydneyTime = sydneyElement.querySelector(".time");
-    let sydneyCurrentTime = moment()
-      .tz("Australia/Sydney")
-      .format("h:mm:ss [<small>]A[</small>]");
+    let sydneyCurrentTime = moment().tz("Australia/Sydney").format(timeFormat);
 
-    sydneyDate.innerHTML = sydneyCurrentDate;
+    sydneyDate.innerHTML = currentDate;
     sydneyTime.innerHTML = sydneyCurrentTime;
+  }
+
+  //Bangui
+  let banguiElement = document.querySelector("#bangui");
+
+  if (banguiElement) {
+    let banguiDate = banguiElement.querySelector(".date");
+    let banguiTime = banguiElement.querySelector(".time");
+    let banguiCurrentTime = moment().tz("Africa/Bangui").format(timeFormat);
+
+    banguiDate.innerHTML = currentDate;
+    banguiTime.innerHTML = banguiCurrentTime;
+  }
+
+  //Tahiti
+  let tahitiElement = document.querySelector("#tahiti");
+
+  if (tahitiElement) {
+    let tahitiDate = tahitiElement.querySelector(".date");
+    let tahitiTime = tahitiElement.querySelector(".time");
+    let tahitiCurrentTime = moment().tz("Pacific/Tahiti").format(timeFormat);
+
+    tahitiDate.innerHTML = currentDate;
+    tahitiTime.innerHTML = tahitiCurrentTime;
   }
 }
 
@@ -47,7 +65,8 @@ function updateCity(event) {
             <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
           </div>
           <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/" class="home">🏠</a>`;
 }
 
 updateTime();
